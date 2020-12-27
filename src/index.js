918167403b35358b23b98dfdda495d7cc9f014c0
Francisco Veloz <franciscoveloz245@gmail.com>
2020-12-27 12:07:28 -0600

    Commit initial con la plantilla para Platzi video...

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+//Importing components
+import HelloWorld from './components/HelloWorld'
+
+ReactDOM.render(<HelloWorld />, document.getElementById("app"))
