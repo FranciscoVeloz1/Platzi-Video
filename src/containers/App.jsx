104e47fb7923b38810528fdacd2376de49ca58c3
Francisco Veloz <franciscoveloz245@gmail.com>
2020-12-27 12:40:55 -0600

    Añadimos los estilos al header

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 //Importing components
 import Header from '../components/Header'
 
+//Importing SASS
+import '../assets/styles/App.scss';
+
 const App = () => (
     <div className="App">
         <Header />
