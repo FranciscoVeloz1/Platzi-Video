2fe5462d525bd919630a9b283be614181ac331c4
Francisco Veloz <franciscoveloz245@gmail.com>
2020-12-30 11:38:34 -0600

    Agregamos PropTypes para asginar los tipos de datos a los props

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -8,40 +8,44 @@ import Carousel from '../components/Carousel'
 import CarouselItem from '../components/CarouselItem'
 import Footer from '../components/Footer'
 
+//Importing Hooks
+import useInitialState from '../hooks/useInitialState'
+
 //Importing SASS
 import '../assets/styles/App.scss';
 
+const API = 'http://localhost:3000/initalState'
+
 const App = () => {
-
-    const [videos, setVideos] = useState({ mylist: [], trends: [], originals: [] })
-
-    useEffect(() => {
-        fetch('http://localhost:3000/initalState')
-            .then(response => response.json())
-            .then(data => setVideos(data))
-    }, [])
-
-    console.log(videos)
-
+    const initialState = useInitialState(API)
     return (
         <div className="App">
             <Header />
             <Search />
 
-            {videos.mylist.length > 0 &&
+            {initialState.mylist.length > 0 &&
                 <Categories title="Mi lista">
                     <Carousel>
-                        <CarouselItem />
+                        {initialState.mylist.map(item =>
+                            <CarouselItem key={item.id} {...item}/>
+                        )}
+
                     </Carousel>
                 </Categories>
             }
 
-
-
             <Categories title="Tendencias">
                 <Carousel>
-                    {videos.trends.map(item =>
-                        <CarouselItem key={item.id} {...item}/>
+                    {initialState.trends.map(item =>
+                        <CarouselItem key={item.id} {...item} />
+                    )}
+                </Carousel>
+            </Categories>
+
+            <Categories title="Originales">
+                <Carousel>
+                    {initialState.originals.map(item =>
+                        <CarouselItem key={item.id} {...item} />
                     )}
                 </Carousel>
             </Categories>
